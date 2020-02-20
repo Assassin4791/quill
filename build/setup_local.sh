32b4b42045e535365eccd045051390d7a319161d
Dyachkov Ivan Alekseevich <i.dyachkov@tinkoff.ru>
2020-02-20 17:56:31 +0700

    rollback scripts

--- build/setup_local.sh
+++ build/setup_local.sh
@@ -6,13 +6,13 @@ set -e
 . /app/build/setup_db_scripts.sh
 
 
-#time setup_mysql $MYSQL_SCRIPT mysql
+time setup_mysql $MYSQL_SCRIPT mysql
 time setup_postgres $POSTGRES_SCRIPT postgres
-#time setup_cassandra $CASSANDRA_SCRIPT cassandra
-#time setup_sqlserver $SQL_SERVER_SCRIPT sqlserver
-#time setup_oracle $ORACLE_SCRIPT oracle
+time setup_cassandra $CASSANDRA_SCRIPT cassandra
+time setup_sqlserver $SQL_SERVER_SCRIPT sqlserver
+time setup_oracle $ORACLE_SCRIPT oracle
 
 # TODO Move this back up to the top. This is failing for now but want mysql to succeed
-#time setup_sqlite $SQLITE_SCRIPT
+time setup_sqlite $SQLITE_SCRIPT
 
 echo "Databases are ready!"
